5ab6adad0f2715183ff26e73644e0c3bed41abba
Daniel Korat <daniel.korat@intel.com>
2025-02-06 05:54:19 -0800

    train depseek prm

--- train-prm/train.sh
+++ train-prm/train.sh
@@ -1,1 +1,1 @@
-torchrun --nproc_per_node 8 --master_port 20001 -m axolotl.cli.train llama-32-1b-prm.yaml # --deepspeed deepspeed_3.json
+torchrun --nproc_per_node 8 --master_port 20001 -m axolotl.cli.train deepseek-r1-1.5b.yaml # --deepspeed deepspeed_3.json
